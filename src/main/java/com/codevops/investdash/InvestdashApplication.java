31b649cb9812d5de77014d4287efe8eaac81a717
Ederson Silva <edersigner@gmail.com>
2022-06-14 13:36:16 -0300

    Criando o projeto da API InvestDash

---
+++ src/main/java/com/codevops/investdash/InvestdashApplication.java
@@ -0,0 +1,13 @@
+package com.codevops.investdash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestdashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestdashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codevops/investdash/InvestdashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codevops.investdash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestdashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
